809ee793d51fab3723152d667f11e22efce41811
Syed Syab Ahmad <81256221+SyabAhmad@users.noreply.github.com>
2023-11-10 22:24:08 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='datafit',
     version='0.2023.2',
     description='This is a Python package that automates your data preprocessing',
-    long_description='This is a Python package that automates your data preprocessing, we will add documentation here on how to use this package',
-    long_description_content_type='text/plain',  # Specify the type of content as Markdown
-    author='Syed Syab & Hamza Rustam',
+    long_description='This is a Python package that automates your data preprocessing we will add documentation here on how to use this package',
+    long_description_content_type='text/plain',
+    author='Syed Syab',
     author_email='syab.se@hotmail.com',
     url='https://github.com/SyabAhmad/datafit',
     license='MIT',
@@ -19,14 +19,12 @@ setup(
 
     ],
 
-    # Entry points for command-line scripts if applicable
     entry_points={
         'console_scripts': [
             'my_script = datafit.datafit:information',
         ],
     },
 
-    # Other optional metadata
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -34,6 +32,5 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        # Add more classifiers as appropriate
     ],
 )

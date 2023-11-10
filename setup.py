80d230c126d0eaca95977eb2daea166e9a2f553f
Syed Syab Ahmad <81256221+SyabAhmad@users.noreply.github.com>
2023-11-10 22:41:02 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='datafit',
-    version='0.2023.2',
+    version='0.2023.1',
     description='This is a Python package that automates your data preprocessing',
-    long_description='This is a Python package that automates your data preprocessing',
-    long_description_content_type='text/plain',
-    author='Syed Syab',
+    long_description='This is a Python package that automates your data preprocessing, we will add documentation here on how to use this package',
+    long_description_content_type='text/plain',  # Specify the type of content as Markdown
+    author='Syed Syab & Hamza Rustam',
     author_email='syab.se@hotmail.com',
     url='https://github.com/SyabAhmad/datafit',
     license='MIT',
+
     packages=find_packages(),
     install_requires=[
         'numpy>=1.0',
@@ -17,11 +18,15 @@ setup(
         'scikit-learn',
 
     ],
+
+    # Entry points for command-line scripts if applicable
     entry_points={
         'console_scripts': [
             'my_script = datafit.datafit:information',
         ],
     },
+
+    # Other optional metadata
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -29,5 +34,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        # Add more classifiers as appropriate
     ],
 )
